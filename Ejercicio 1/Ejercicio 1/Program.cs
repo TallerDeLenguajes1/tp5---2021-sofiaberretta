6eb03d4a25133374cdff2ba1630981d995989d15
Ana Sofia Berretta Gali <anasofiaberrettagali@gmail.com>
2021-06-15 10:05:40 -0300

    Agregado ejercicio 1

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, numInvertido=0;
+            Console.WriteLine("## Invirtiendo un numero ##");
+            Console.WriteLine("\n Ingrese el numero a invertir:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if(numero > 0)
+            {
+                while(numero >= 10)
+                {
+                    numInvertido = (numInvertido * 10) + (numero % 10);
+                    numero /= 10;
+                }
+
+                numInvertido = (numInvertido * 10) + numero;
+                Console.WriteLine("\nEl numero invertido es " + numInvertido);
+            }
+            else
+            {
+                Console.WriteLine("\nNo se puede realizar la inversion");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
